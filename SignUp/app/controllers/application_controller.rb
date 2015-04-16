13eeea0446eb10aeb99b65f8fe7353bc2acaa150
Dustin <dustin.t.baker22@gmail.com>
2015-04-16 08:58:03 -0700

    Created a login... And it didn't even BREAK! ;o)

--- SignUp/app/controllers/application_controller.rb
+++ SignUp/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :require_user
+  helper_method :current_user, :require_user, :require_admin
 
   def current_user
   	if session[:user_id]
@@ -11,11 +11,15 @@ class ApplicationController < ActionController::Base
   			return @current_user
   		else
   			return nil
-  		end
+  	end
   end
 
   def require_user
   	redirect_to '/login' unless current_user
+  end
+
+  def require_admin
+    redirect_to '/login' unless current_user.admin?
   end
 
 end
--- SignUp/app/controllers/pages_controller.rb
+++ SignUp/app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
 before_action :require_user, except: [:contact, :help]
+before_action :require_admin, only: [:help]
 
 def signup
 
