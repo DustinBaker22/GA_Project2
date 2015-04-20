001b20ec627d76c9a7c6e9efe63d1646318a2f61
Dustin <dustin.t.baker22@gmail.com>
2015-04-19 19:35:44 -0700

    Completed some minor things with login and signup, and also got the font to show up in black on the login page within the input boxes

--- SignUp/app/controllers/pages_controller.rb
+++ SignUp/app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
 before_action :require_user, except: [:contact, :help]
 before_action :require_admin, only: [:help]
 
+def index
+
+end
+
 def signup
 
 end
--- SignUp/config/routes.rb
+++ SignUp/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => 'sessions#new'
 	get '/signup' => 'users#new'
 	#root "pages#signup"
 	post "users" => "users#create"
